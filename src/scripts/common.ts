f73af4b8231334593abca8b63608e61bce350ed8
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2024-10-27 03:17:34 +0100

    small fixes

--- src/scripts/common.ts
+++ src/scripts/common.ts
@@ -1,5 +1,5 @@
+if (localStorage.getItem('darkmode') === null) localStorage.setItem('darkmode', '1');
 let darkmode = localStorage.getItem('darkmode') === '1';
-if (!darkmode) localStorage.setItem('darkmode', '1');
 loadDarkmode();
 (document.querySelector(`a[href="${window.location.pathname.split('/').pop() || 'index.html'}"]`) as HTMLElement).classList.add('underline');
 
